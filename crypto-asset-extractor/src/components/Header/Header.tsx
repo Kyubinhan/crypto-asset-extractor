89cf3c3dd45dc5ee5dad4831e11e0433b67a01ce
Kyubin Han <kyubinhan11@gmail.com>
2023-04-30 22:03:49 +0900

    Add styling to Header component

--- crypto-asset-extractor/src/components/Header/Header.tsx
+++ crypto-asset-extractor/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import styles from "./style.module.scss";
+
 const Header = () => {
   return (
-    <div>
-      <h2>크립토 자산 데이터 추출하기</h2>
-      <h4>
+    <div className={styles.header}>
+      <h1>크립토 자산 데이터 추출하기</h1>
+      <h2>
         데이터 추출이 필요한 크립토 자산을 선택 후, 데이터 추출이 가능합니다.
-      </h4>
+      </h2>
     </div>
   );
 };
