d4fa5dd6299410d89b0b33574f1cfb33df756143
Kyubin Han <kyubinhan11@gmail.com>
2023-04-30 19:14:44 +0900

    Implement stop watch logic in StopWatch component

--- src/components/StopWatch/StopWatch.tsx
+++ src/components/StopWatch/StopWatch.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   handleStart: () => void;
@@ -6,17 +6,45 @@ interface Props {
 }
 
 const StopWatch: React.FC<Props> = ({ handleStart, handleReset }) => {
+  const [time, setTime] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timer | undefined = undefined;
+
+    if (isCounting) {
+      intervalId = setInterval(() => {
+        setTime((t) => t + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isCounting]);
+
   const onStartClick = () => {
+    setIsCounting(true);
     handleStart();
   };
   const onResetClick = () => {
+    setIsCounting(false);
+    setTime(0);
     handleReset();
   };
 
+  const hours = String(Math.floor(time / 3600)).padStart(2, "0");
+  const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, "0");
+  const seconds = String(time % 60).padStart(2, "0");
+
   return (
     <div>
-      <button onClick={onStartClick}>Start</button>
-      <button onClick={onResetClick}>Reset</button>
+      {hours}:{minutes}:{seconds}
+      {isCounting ? (
+        <button onClick={onResetClick}>Reset</button>
+      ) : (
+        <button onClick={onStartClick}>Start</button>
+      )}
     </div>
   );
 };
