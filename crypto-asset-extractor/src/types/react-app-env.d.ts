769b4e759f693bbae1a4ee3b0ac401609704b0f8
Kyubin Han <kyubinhan11@gmail.com>
2023-05-01 12:32:21 +0900

    Remove unused global type definition

--- crypto-asset-extractor/src/types/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production";
-    PUBLIC_URL: string;
-    REACT_APP_CMC_URL: string;
-    REACT_APP_CMC_API_KEY: string;
-  }
-}
